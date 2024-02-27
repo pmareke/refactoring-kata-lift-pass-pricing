9fb95122c6165c45b31d183f759f0f806d5fa237
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-02-27 21:30:18 +0100

    feat: refactor tests

--- lift-pass/test/acceptance/test_1jour_prices.py
+++ lift-pass/test/acceptance/test_1jour_prices.py
@@ -10,47 +10,45 @@ class Test1JourPricesAcceptance:
 
     def test_1jour_type(self) -> None:
         lift_type = LyftType.JOUR
-        response = self.client.get("/prices", query_string={"type": lift_type.value})
+
+        response = self.client.get(f"/prices?type={lift_type.value}")
 
         expect(response.json).to(equal({"cost": 35}))
 
     def test_people_under_6_not_pay(self) -> None:
         lift_type = LyftType.JOUR
-        response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "age": 5}
-        )
+
+        response = self.client.get(f"/prices?type={lift_type.value}&age=5")
 
         expect(response.json).to(equal({"cost": 0}))
 
     def test_1jour_type_with_age_bellow_15(self) -> None:
         lift_type = LyftType.JOUR
-        response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "age": 14}
-        )
+
+        response = self.client.get(f"/prices?type={lift_type.value}&age=14")
 
         expect(response.json).to(equal({"cost": 25}))
 
     def test_1jour_type_with_age_between_16_and_63(self) -> None:
         lift_type = LyftType.JOUR
-        response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "age": 50}
-        )
+
+        response = self.client.get(f"/prices?type={lift_type.value}&age=50")
 
         expect(response.json).to(equal({"cost": 35}))
 
     def test_1jour_type_with_age_above_64(self) -> None:
         lift_type = LyftType.JOUR
-        response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "age": 65}
-        )
+
+        response = self.client.get(f"/prices?type={lift_type.value}&age=65")
 
         expect(response.json).to(equal({"cost": 27}))
 
     def test_1jour_type_on_holidays(self) -> None:
         lift_type = LyftType.JOUR
         holiday_date = "2019-02-18"
+
         response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "date": holiday_date}
+            f"/prices?type={lift_type.value}&date={holiday_date}"
         )
 
         expect(response.json).to(equal({"cost": 35}))
@@ -58,8 +56,9 @@ class Test1JourPricesAcceptance:
     def test_1jour_type_not_on_holidays(self) -> None:
         lift_type = LyftType.JOUR
         non_holiday_date = "2024-02-26"
+
         response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "date": non_holiday_date}
+            f"/prices?type={lift_type.value}&date={non_holiday_date}"
         )
 
         expect(response.json).to(equal({"cost": 23}))
--- lift-pass/test/acceptance/test_night_prices.py
+++ lift-pass/test/acceptance/test_night_prices.py
@@ -10,30 +10,28 @@ class TestNightPricesAcceptance:
 
     def test_nights_are_free(self) -> None:
         lift_type = LyftType.NIGHT
-        response = self.client.get("/prices", query_string={"type": lift_type.value})
+
+        response = self.client.get(f"/prices?type={lift_type.value}")
 
         expect(response.json).to(equal({"cost": 0}))
 
     def test_people_under_6_not_pay(self) -> None:
         lift_type = LyftType.NIGHT
-        response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "age": 5}
-        )
+
+        response = self.client.get(f"/prices?type={lift_type.value}&age=5")
 
         expect(response.json).to(equal({"cost": 0}))
 
     def test_nights_with_age_above_6_has_not_discount(self) -> None:
         lift_type = LyftType.NIGHT
-        response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "age": 40}
-        )
+
+        response = self.client.get(f"/prices?type={lift_type.value}&age=40")
 
         expect(response.json).to(equal({"cost": 19}))
 
     def test_nights_with_age_above_64_has_40_percentage_discount(self) -> None:
         lift_type = LyftType.NIGHT
-        response = self.client.get(
-            "/prices", query_string={"type": lift_type.value, "age": 65}
-        )
+
+        response = self.client.get(f"/prices?type={lift_type.value}&age=65")
 
         expect(response.json).to(equal({"cost": 8}))
