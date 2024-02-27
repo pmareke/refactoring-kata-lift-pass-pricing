230ea19c24f8712f2fe152e30d259bd76cb223ee
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-02-27 15:33:04 +0100

    feat: refactor code

--- lift-pass/src/use_cases/get_price_query_handler.py
+++ lift-pass/src/use_cases/get_price_query_handler.py
@@ -17,69 +17,69 @@ class GetPriceQueryHandler:
         self.cursor = cursor
 
     def execute(self, query: GetPriceQuery) -> dict:
-        response = {"cost": 0}
         row = self._fetch_one_trip(query.trip_type)
         result = {"cost": row[0]}
 
         if query.age and query.age < 6:
-            response["cost"] = 0
-            return response
+            return {"cost": 0}
 
         if query.trip_type == "night":
-            self._calculate_cost_for_night(query, response, result)
-        else:
-            self._calculate_cost_for_non_nights(query, response, result)
-        return response
+            return self._calculate_cost_for_night(query, result)
+
+        return self._calculate_cost_for_non_nights(query, result)
 
     def _fetch_one_trip(self, trip_type: str):
         self.cursor.execute(f"SELECT cost FROM base_price WHERE type = ? ", trip_type)
         return self.cursor.fetchone()
 
     @staticmethod
-    def _calculate_cost_for_night(
-        query: GetPriceQuery, response: dict, result: dict
-    ) -> None:
-        if query.age and query.age >= 6:
-            if query.age > 64:
-                response["cost"] = math.ceil(result["cost"] * 0.4)
-            else:
-                response.update(result)
-        else:
-            response["cost"] = 0
+    def _calculate_cost_for_night(query: GetPriceQuery, result: dict) -> dict:
+        if not query.age:
+            return {"cost": 0}
+        if 6 < query.age and query.age > 64:
+            return {"cost": math.ceil(result["cost"] * 0.4)}
+        return result
 
     def _calculate_cost_for_non_nights(
-        self, query: GetPriceQuery, response: dict, result: dict
-    ):
-        self.cursor.execute("SELECT * FROM holidays")
+        self, query: GetPriceQuery, result: dict
+    ) -> dict:
+        response = {"cost": 0}
         is_holiday = False
         reduction = 0
-        for row in self.cursor.fetchall():
-            holiday = row[0]
-            if query.date:
-                d = datetime.fromisoformat(query.date)
-                if (
-                    d.year == holiday.year
-                    and d.month == holiday.month
-                    and holiday.day == d.day
-                ):
-                    is_holiday = True
+        self.cursor.execute("SELECT * FROM holidays")
+        holidays = self.cursor.fetchall()
+        for holiday in holidays:
+            holiday = holiday[0]
+            if not query.date:
+                continue
+
+            d = datetime.fromisoformat(query.date)
+            if (
+                d.year == holiday.year
+                and d.month == holiday.month
+                and holiday.day == d.day
+            ):
+                is_holiday = True
+
         if (
             not is_holiday
             and query.date
             and datetime.fromisoformat(query.date).weekday() == 0
         ):
             reduction = 35
+
         # TODO: apply reduction for others
         if query.age and query.age < 15:
-            response["cost"] = math.ceil(result["cost"] * 0.7)
-        else:
-            if not query.age:
-                cost = result["cost"] * (1 - reduction / 100)
-                response["cost"] = math.ceil(cost)
-            else:
-                if query.age and query.age > 64:
-                    cost = result["cost"] * 0.75 * (1 - reduction / 100)
-                    response["cost"] = math.ceil(cost)
-                elif query.age:
-                    cost = result["cost"] * (1 - reduction / 100)
-                    response["cost"] = math.ceil(cost)
+            return {"cost": math.ceil(result["cost"] * 0.7)}
+
+        if not query.age:
+            cost = result["cost"] * (1 - reduction / 100)
+            return {"cost": math.ceil(cost)}
+
+        if query.age > 64:
+            cost = result["cost"] * 0.75 * (1 - reduction / 100)
+            return {"cost": math.ceil(cost)}
+
+        cost = result["cost"] * (1 - reduction / 100)
+        response["cost"] = math.ceil(cost)
+        return response
