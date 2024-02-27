b3caa2374084edda37913aadeabba31444b19d26
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-02-27 10:30:59 +0100

    feat: refactor prices

--- lift-pass/src/prices.py
+++ lift-pass/src/prices.py
@@ -38,62 +38,74 @@ def get_price():
     return _get_price(connection)
 
 
+def _calculate_cost_for_night(response: dict, result: dict) -> None:
+    if "age" in request.args and request.args.get("age", type=int) >= 6:
+        if request.args.get("age", type=int) > 64:
+            response["cost"] = math.ceil(result["cost"] * 0.4)
+        else:
+            response.update(result)
+    else:
+        response["cost"] = 0
+
+
+def _fetch_one_trip(conn: Connection, trip_type: str):
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT cost FROM base_price WHERE type = ? ", trip_type)
+    return cursor.fetchone()
+
+
 def _get_price(conn: Connection) -> dict:
     res = {}
-    cursor = conn.cursor()
-    cursor.execute(
-        f"SELECT cost FROM base_price " + "WHERE type = ? ", (request.args["type"],)
-    )
-    row = cursor.fetchone()
+    trip_type = request.args["type"]
+    row = _fetch_one_trip(conn, trip_type)
     result = {"cost": row[0]}
+
     if "age" in request.args and request.args.get("age", type=int) < 6:
         res["cost"] = 0
-    else:
-        if "type" in request.args and request.args["type"] != "night":
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM holidays")
-            is_holiday = False
-            reduction = 0
-            for row in cursor.fetchall():
-                holiday = row[0]
-                if "date" in request.args:
-                    d = datetime.fromisoformat(request.args["date"])
-                    if (
-                        d.year == holiday.year
-                        and d.month == holiday.month
-                        and holiday.day == d.day
-                    ):
-                        is_holiday = True
-            if (
-                not is_holiday
-                and "date" in request.args
-                and datetime.fromisoformat(request.args["date"]).weekday() == 0
-            ):
-                reduction = 35
+        return res
 
-            # TODO: apply reduction for others
-            if "age" in request.args and request.args.get("age", type=int) < 15:
-                res["cost"] = math.ceil(result["cost"] * 0.7)
-            else:
-                if "age" not in request.args:
-                    cost = result["cost"] * (1 - reduction / 100)
-                    res["cost"] = math.ceil(cost)
-                else:
-                    if "age" in request.args and request.args.get("age", type=int) > 64:
-                        cost = result["cost"] * 0.75 * (1 - reduction / 100)
-                        res["cost"] = math.ceil(cost)
-                    elif "age" in request.args:
-                        cost = result["cost"] * (1 - reduction / 100)
-                        res["cost"] = math.ceil(cost)
-        else:
-            if "age" in request.args and request.args.get("age", type=int) >= 6:
-                if request.args.get("age", type=int) > 64:
-                    res["cost"] = math.ceil(result["cost"] * 0.4)
-                else:
-                    res.update(result)
-            else:
-                res["cost"] = 0
+    if trip_type == "night":
+        _calculate_cost_for_night(res, result)
+    else:
+        _calculate_cost_for_non_nights(conn, res, result)
     return res
+
+
+def _calculate_cost_for_non_nights(conn: Connection, response: dict, result: dict):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM holidays")
+    is_holiday = False
+    reduction = 0
+    for row in cursor.fetchall():
+        holiday = row[0]
+        if "date" in request.args:
+            d = datetime.fromisoformat(request.args["date"])
+            if (
+                    d.year == holiday.year
+                    and d.month == holiday.month
+                    and holiday.day == d.day
+            ):
+                is_holiday = True
+    if (
+            not is_holiday
+            and "date" in request.args
+            and datetime.fromisoformat(request.args["date"]).weekday() == 0
+    ):
+        reduction = 35
+    # TODO: apply reduction for others
+    if "age" in request.args and request.args.get("age", type=int) < 15:
+        response["cost"] = math.ceil(result["cost"] * 0.7)
+    else:
+        if "age" not in request.args:
+            cost = result["cost"] * (1 - reduction / 100)
+            response["cost"] = math.ceil(cost)
+        else:
+            if "age" in request.args and request.args.get("age", type=int) > 64:
+                cost = result["cost"] * 0.75 * (1 - reduction / 100)
+                response["cost"] = math.ceil(cost)
+            elif "age" in request.args:
+                cost = result["cost"] * (1 - reduction / 100)
+                response["cost"] = math.ceil(cost)
 
 
 if __name__ == "__main__":
