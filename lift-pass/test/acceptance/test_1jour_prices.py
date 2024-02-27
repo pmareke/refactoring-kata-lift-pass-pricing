dafe390d8b60c86eb0b058e3cd22598ec21ed605
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-02-27 09:17:35 +0100

    feat: refactor tests

--- lift-pass/test/acceptance/test_1jour_prices.py
+++ lift-pass/test/acceptance/test_1jour_prices.py
@@ -4,35 +4,19 @@ from src.prices import app
 from expects import expect, equal
 
 
-class TestPricesAcceptance:
+class Test1JourPricesAcceptance:
     def setup_method(self) -> None:
         self.client = app.test_client()
-
-    @pytest.mark.parametrize("trip_type", ["1jour", "night"])
-    def test_people_under_6_not_pay(self, trip_type: str) -> None:
-        response = self.client.get("/prices", query_string={"type": trip_type, "age": 5})
-
-        expect(response.json).to(equal({"cost": 0}))
-
-    def test_nights_are_free(self) -> None:
-        response = self.client.get("/prices", query_string={"type": "night"})
-
-        expect(response.json).to(equal({"cost": 0}))
-
-    def test_nights_with_age_above_6(self) -> None:
-        response = self.client.get("/prices", query_string={"type": "night", "age": 40})
-
-        expect(response.json).to(equal({"cost": 19}))
-
-    def test_nights_with_age_above_64(self) -> None:
-        response = self.client.get("/prices", query_string={"type": "night", "age": 65})
-
-        expect(response.json).to(equal({"cost": 8}))
 
     def test_1jour_type(self) -> None:
         response = self.client.get("/prices", query_string={"type": "1jour"})
 
         expect(response.json).to(equal({"cost": 35}))
+
+    def test_people_under_6_not_pay(self) -> None:
+        response = self.client.get("/prices", query_string={"type": "1jour", "age": 5})
+
+        expect(response.json).to(equal({"cost": 0}))
 
     def test_1jour_type_with_age_bellow_15(self) -> None:
         response = self.client.get("/prices", query_string={"type": "1jour", "age": 14})
@@ -60,8 +44,3 @@ class TestPricesAcceptance:
         response = self.client.get("/prices", query_string={"type": "1jour", "date": date})
 
         expect(response.json).to(equal({"cost": 23}))
-
-    def test_1jour_price(self) -> None:
-        response = self.client.get("/prices", query_string={"type": "1jour"})
-
-        expect(response.json).to(equal({"cost": 35}))
---
+++ lift-pass/test/acceptance/test_night_prices.py
@@ -0,0 +1,29 @@
+import pytest 
+
+from src.prices import app
+from expects import expect, equal
+
+
+class TestNightPricesAcceptance:
+    def setup_method(self) -> None:
+        self.client = app.test_client()
+
+    def test_nights_are_free(self) -> None:
+        response = self.client.get("/prices", query_string={"type": "night"})
+
+        expect(response.json).to(equal({"cost": 0}))
+
+    def test_people_under_6_not_pay(self) -> None:
+        response = self.client.get("/prices", query_string={"type": "night", "age": 5})
+
+        expect(response.json).to(equal({"cost": 0}))
+
+    def test_nights_with_age_above_6(self) -> None:
+        response = self.client.get("/prices", query_string={"type": "night", "age": 40})
+
+        expect(response.json).to(equal({"cost": 19}))
+
+    def test_nights_with_age_above_64(self) -> None:
+        response = self.client.get("/prices", query_string={"type": "night", "age": 65})
+
+        expect(response.json).to(equal({"cost": 8}))
