43652fce654f4a39a4b21d9d9cb71a43eb118ddb
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-02-27 09:24:11 +0100

    feat: refactor prices

--- lift-pass/src/prices.py
+++ lift-pass/src/prices.py
@@ -3,6 +3,9 @@ import math
 from flask import Flask
 from flask import request
 from datetime import datetime
+
+from pymysql import Connection
+
 from src.db import create_lift_pass_db_connection
 
 app = Flask("lift-pass-pricing")
@@ -14,84 +17,90 @@ connection_options = {
     "password": "mysql",
 }
 
-connection = None
+connection = create_lift_pass_db_connection(connection_options)
 
 
-@app.route("/prices", methods=["GET", "PUT"])
-def prices():
+@app.put("/prices")
+def update_prices():
+    return _update_prices(connection)
+
+
+@app.get("/prices")
+def get_prices():
+    return _get_price(connection)
+
+
+def _get_price(conn: Connection) -> dict:
     res = {}
-    global connection
-    if connection is None:
-        connection = create_lift_pass_db_connection(connection_options)
-    if request.method == "PUT":
-        lift_pass_cost = request.args["cost"]
-        lift_pass_type = request.args["type"]
-        cursor = connection.cursor()
-        cursor.execute(
-            "INSERT INTO `base_price` (type, cost) VALUES (?, ?) "
-            + "ON DUPLICATE KEY UPDATE cost = ?",
-            (lift_pass_type, lift_pass_cost, lift_pass_cost),
-        )
-        return {}
-    elif request.method == "GET":
-        cursor = connection.cursor()
-        cursor.execute(
-            f"SELECT cost FROM base_price " + "WHERE type = ? ", (request.args["type"],)
-        )
-        row = cursor.fetchone()
-        result = {"cost": row[0]}
-        if "age" in request.args and request.args.get("age", type=int) < 6:
-            res["cost"] = 0
-        else:
-            if "type" in request.args and request.args["type"] != "night":
-                cursor = connection.cursor()
-                cursor.execute("SELECT * FROM holidays")
-                is_holiday = False
-                reduction = 0
-                for row in cursor.fetchall():
-                    holiday = row[0]
-                    if "date" in request.args:
-                        d = datetime.fromisoformat(request.args["date"])
-                        if (
+    cursor = conn.cursor()
+    cursor.execute(
+        f"SELECT cost FROM base_price " + "WHERE type = ? ", (request.args["type"],)
+    )
+    row = cursor.fetchone()
+    result = {"cost": row[0]}
+    if "age" in request.args and request.args.get("age", type=int) < 6:
+        res["cost"] = 0
+    else:
+        if "type" in request.args and request.args["type"] != "night":
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM holidays")
+            is_holiday = False
+            reduction = 0
+            for row in cursor.fetchall():
+                holiday = row[0]
+                if "date" in request.args:
+                    d = datetime.fromisoformat(request.args["date"])
+                    if (
                             d.year == holiday.year
                             and d.month == holiday.month
                             and holiday.day == d.day
-                        ):
-                            is_holiday = True
-                if (
+                    ):
+                        is_holiday = True
+            if (
                     not is_holiday
                     and "date" in request.args
                     and datetime.fromisoformat(request.args["date"]).weekday() == 0
-                ):
-                    reduction = 35
+            ):
+                reduction = 35
 
-                # TODO: apply reduction for others
-                if "age" in request.args and request.args.get("age", type=int) < 15:
-                    res["cost"] = math.ceil(result["cost"] * 0.7)
+            # TODO: apply reduction for others
+            if "age" in request.args and request.args.get("age", type=int) < 15:
+                res["cost"] = math.ceil(result["cost"] * 0.7)
+            else:
+                if "age" not in request.args:
+                    cost = result["cost"] * (1 - reduction / 100)
+                    res["cost"] = math.ceil(cost)
                 else:
-                    if "age" not in request.args:
-                        cost = result["cost"] * (1 - reduction / 100)
-                        res["cost"] = math.ceil(cost)
-                    else:
-                        if (
+                    if (
                             "age" in request.args
                             and request.args.get("age", type=int) > 64
-                        ):
-                            cost = result["cost"] * 0.75 * (1 - reduction / 100)
-                            res["cost"] = math.ceil(cost)
-                        elif "age" in request.args:
-                            cost = result["cost"] * (1 - reduction / 100)
-                            res["cost"] = math.ceil(cost)
-            else:
-                if "age" in request.args and request.args.get("age", type=int) >= 6:
-                    if request.args.get("age", type=int) > 64:
-                        res["cost"] = math.ceil(result["cost"] * 0.4)
-                    else:
-                        res.update(result)
+                    ):
+                        cost = result["cost"] * 0.75 * (1 - reduction / 100)
+                        res["cost"] = math.ceil(cost)
+                    elif "age" in request.args:
+                        cost = result["cost"] * (1 - reduction / 100)
+                        res["cost"] = math.ceil(cost)
+        else:
+            if "age" in request.args and request.args.get("age", type=int) >= 6:
+                if request.args.get("age", type=int) > 64:
+                    res["cost"] = math.ceil(result["cost"] * 0.4)
                 else:
-                    res["cost"] = 0
-
+                    res.update(result)
+            else:
+                res["cost"] = 0
     return res
+
+
+def _update_prices(conn):
+    lift_pass_cost = request.args["cost"]
+    lift_pass_type = request.args["type"]
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO `base_price` (type, cost) VALUES (?, ?) "
+        + "ON DUPLICATE KEY UPDATE cost = ?",
+        (lift_pass_type, lift_pass_cost, lift_pass_cost),
+    )
+    return {}
 
 
 if __name__ == "__main__":
